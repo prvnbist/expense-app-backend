e0be4f1b9f55a9867af8a85920ae6d2069d139b4
Praveen Bisht <prvnbist@gmail>
2018-10-23 21:37:11 +0530

    Updated required fields

--- server/schema/types/User.js
+++ server/schema/types/User.js
@@ -1,11 +1,10 @@
 export const typeDefs = `
 	type User {
-		id: ID
-		name: String
-		email: String
-		income: String
-		username: String
-		password: String
+		id: ID!
+		name: String!
+		email: String!
+		username: String!
+		password: String!
 		# createdAt: String
 		# updatedAt: String
 	}
