fd22ae01874046a6b05c9f2e3fa7600286f6f030
Praveen Bisht <prvnbist@gmail>
2018-10-23 21:36:45 +0530

    Added login & signup resolvers

--- server/schema/resolvers/Resolvers.js
+++ server/schema/resolvers/Resolvers.js
@@ -1,19 +1,47 @@
+const bcrypt = require('bcrypt');
+const jsonwebtoken = require('jsonwebtoken');
+
 import User from '../../models/user';
 
 export const resolvers = {
     Query: {
-        users: (parent, args) => User.find({})
+        me: async(_, args, {user}) => {
+            if (!user) {
+                throw new Error('You are not authenticated!')
+            }
+            return await User.findById(user.id)
+        }
     },
     Mutation: {
-        addUser: (_, {name,email,password,income,username}) => {
-			let user = new User({
-				name,
+        signup: async(_, {name, username, email, password}) => {
+            const user = await User.create({
+                name,
+                username,
                 email,
-                password,
-                income,
-                username 
-			});
-			return user.save();
-		}
+                password: await bcrypt.hash(password, 10)
+            })
+
+            // return json web token
+            return jsonwebtoken.sign({
+                id: user.id,
+                email: user.email
+            }, 'secret', {expiresIn: '1y'})
+        },
+
+        login: async(_, {username, password}) => {
+            const user = await User.findOne({username});
+            if (!user) {
+                throw new Error('No user with that username')
+            }
+            const valid = await bcrypt.compare(password, user.password);
+            if (!valid) {
+                throw new Error('Incorrect password')
+            }
+
+            return jsonwebtoken.sign({
+                _id: user.id,
+                email: user.email
+            }, 'secret', {expiresIn: '1y'});
+        }
     }
 };
