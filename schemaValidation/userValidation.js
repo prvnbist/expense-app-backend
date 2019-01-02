623980e2503baa64d35573e685e51f3719ce1f57
Praveen Bisht <prvnbist@gmail.com>
2019-01-02 15:47:55 +0530

    Added regex to username for _apha_numerics

--- schemaValidation/userValidation.js
+++ schemaValidation/userValidation.js
@@ -2,7 +2,7 @@ import Joi from 'joi';
 
 export default Joi.object().keys({
     email:Joi.string().email().required().label('Email'),
-    username:Joi.string().alphanum().min(4).max(30).required().label('Username'),
+    username:Joi.string().regex(/^[a-zA-Z0-9-_]+$/).min(4).max(30).required().label('Username'),
     password:Joi.string().regex(/^(?=S*[a-z])(?=S*[A-Z])(?=\S*\d)(?=S*[^\W\s])\S{8,30}$/i).label('Password').options({
         language: {
             string: {
