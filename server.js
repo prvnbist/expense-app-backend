2d0a899d5fa81826f2496ac9b0d3560def849b1e
Praveen Bisht <prvnbist@gmail.com>
2019-07-10 23:00:46 +0530

    Refactor: Removed redundant code

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import {ApolloServer} from 'apollo-server-express';
-import mongoose from 'mongoose';
-import express from 'express';
-import cors from 'cors';
+import { ApolloServer } from "apollo-server-express";
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
 
 // GraphQL Schema
-import {schema} from './schema/schema';
+import { schema } from "./schema/schema";
 
 // Authentication Functions
-import {verifyUser} from './auth/Auth';
+import { verifyUser } from "./auth/Auth";
 
 // Create Express App
 const app = express();
@@ -15,21 +15,25 @@ const app = express();
 // Allow Cross-Origin Requests
 app.use(cors());
 
+require("dotenv").config();
 // Config/Connect to DB
-const db = require("./config/keys").mongoURI;
-mongoose.set('useCreateIndex', true);
+mongoose.set("useCreateIndex", true);
 mongoose
-    .connect(db, {useNewUrlParser: true})
-    .then(() => console.log("Connected to MongoDB!"))
-    .catch(err => console.log(err));
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch(err => console.log(err));
 
 //Create Apollo Server
 const server = new ApolloServer({
-    schema,
-    context: ({req, res}) => ({userId: verifyUser(req), res})
+  schema,
+  context: ({ req, res }) => ({ userId: verifyUser(req), res })
 });
 
-server.applyMiddleware({app});
-app.listen({
+server.applyMiddleware({ app });
+app.listen(
+  {
     port: process.env.PORT || 4000
-}, () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`));
+  },
+  () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
